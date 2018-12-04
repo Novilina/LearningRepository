36b99aced5390fe6cc4f48990cccc4a64facd99f
Galina Novikova <sollinka@mail.ru>
2018-12-04 21:12:51 +0300

    Little acquaintance

---
+++ src/HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+	class InputConsoleDemo
+	{
+		static void Main()
+		{
+			string name;
+			Console.Title = "Давайте познакомимся...";
+			Console.Write("Пожалуйста, представьтесь");
+			name = Console.ReadLine();
+			string txt = "Очень приятно, " + name + "!";
+			Console.Title = "Знакомство прошло успешно. Поздравляем!";
+			Console.WriteLine(txt);
+			Console.ReadLine();
+		}
+	}
+}
